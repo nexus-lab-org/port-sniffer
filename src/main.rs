3725a48b410de11ac1d42032aafbce009c375905
MaxAsif <asifiqbal7.iitkgp@gmail.com>
2023-08-18 10:19:04 +0530

    feat: ip port sniffer

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use std::net::{IpAddr, TcpStream};
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
+use std::io::{self, Write};
+use clap::Parser;
+
+const MAX_PORT: u32 = 65535;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short='i', long="ip")]
+    ip: IpAddr,
+    #[arg(short='t', long="threads", default_value_t = 10)]
+    threads: u32,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    println!("Scanning {} with {:?} threads", cli.ip, cli.threads);
+    let (tx, rx) = channel::<u32>();
+
+    for i in 0..cli.threads {
+        let tx = tx.clone();
+        thread::spawn(move || {
+            scan(tx, i, cli.ip, cli.threads);
+        });
+    }
+    drop(tx);
+    let mut open: Vec<u32> = rx.iter().collect();
+
+    println!("");
+    open.sort();
+    for port in open {
+        println!("{} is open", port);
+    }
+}
+
+fn scan(tx: Sender<u32>, start_port: u32, addr: IpAddr, threads: u32) {
+    let mut port: u32 = start_port + 1;
+    loop {
+        let address = format!("{}:{}", addr, port);
+        match TcpStream::connect(address) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+        if (MAX_PORT - port) <= threads {
+            break;
+        }
+        port += threads;
+    }
+}
