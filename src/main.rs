fad7f7873b9faa52d36799d4324e6a7e0306a865
0Ky <16103757+0Ky@users.noreply.github.com>
2023-10-07 06:25:36 +1100

    feat(cli): added progress readout and handled SIGINT

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use clap::Parser;
+use ctrlc;
 use nexuslab_port_sniffer::constants::{DEFAULT_THREADS, DEFAULT_TIMEOUT, MAX_PORT, MIN_PORT};
 use nexuslab_port_sniffer::models::{IpOrDomain, Ports};
 use std::io::{self, Write};
 use std::net::{IpAddr, TcpStream, ToSocketAddrs};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::mpsc::{channel, Sender};
 use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
@@ -49,20 +52,49 @@ fn main() {
     println!("Scanning {} with {:?} threads", ip_addr, cli.threads);
 
     let (tx, rx) = channel::<u32>();
+    let rx = Arc::new(Mutex::new(rx));
+    let rx_clone = rx.clone();
+
+    ctrlc::set_handler(move || {
+        print!("\nReceived <Ctrl+C> scan halted, displaying results...");
+        display_results(rx_clone.lock().unwrap().try_iter().collect());
+        std::process::exit(0);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    let ports_scanned = Arc::new(AtomicUsize::new(0));
+    let mut handles = Vec::new();
 
     for i in 0..cli.threads {
         let tx = tx.clone();
         let ports_to_scan = ports_to_scan.clone();
-        thread::spawn(move || {
-            scan(tx, ports_to_scan, i, ip_addr, cli.threads, cli.timeout);
+        let ports_scanned = ports_scanned.clone();
+        let handle = thread::spawn(move || {
+            scan(
+                tx,
+                ports_to_scan,
+                i,
+                ip_addr,
+                cli.threads,
+                cli.timeout,
+                ports_scanned,
+            );
         });
+        handles.push(handle);
     }
     drop(tx);
-    let mut open: Vec<u32> = rx.iter().collect();
 
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    display_results(rx.lock().unwrap().iter().collect());
+}
+
+fn display_results(open_ports: Vec<u32>) {
     println!();
+    let mut open = open_ports;
     open.sort();
-
     if open.is_empty() {
         println!("None of the analysed ports were open");
     } else {
@@ -79,6 +111,7 @@ fn scan(
     addr: IpAddr,
     threads: u32,
     timeout: u64,
+    ports_scanned: Arc<AtomicUsize>,
 ) {
     // This function scans ports at positions
     // start_port_index,
@@ -88,8 +121,9 @@ fn scan(
 
     let duration = Duration::new(timeout, 0);
     let mut port_index = start_port_index;
+    let total_ports = ports_to_scan.len();
     loop {
-        if port_index >= ports_to_scan.len() as u32 {
+        if port_index >= total_ports as u32 {
             break;
         }
         let port = ports_to_scan[port_index as usize];
@@ -100,6 +134,17 @@ fn scan(
             io::stdout().flush().unwrap();
             tx.send(port).unwrap();
         }
+
+        let scanned = ports_scanned.fetch_add(1, Ordering::Relaxed) + 1;
+        let progress = (scanned as f64 / total_ports as f64) * 100.0;
+
+        print!(
+            "\rProgress: {:.2}% (Scanned {} out of {})",
+            progress, scanned, total_ports
+        );
+
+        io::stdout().flush().unwrap();
+
         port_index += threads;
     }
 }
